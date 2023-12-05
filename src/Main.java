8c5fb287e3d694df46076e06111003fdd3bff207
Luna3Chiara <luna@anche.no>
2023-12-05 12:06:26 +0100

    finito esercizio 2

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //il mio oggetto rettangolo
+        Rettangolo rettangolo = new Rettangolo(5.0, 3.0);
+
+        //chiamo al metodo calcolaArea x classe rettangolo
+        rettangolo.calcolaArea();  //stampami area del triangolo
+    }
+}
